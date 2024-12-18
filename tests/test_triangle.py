34dd0adc60a662cbcdf98ce646d1bbd50b4730a7
de0Gusman <471735@niuitmo.ru>
2024-12-18 19:09:10 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -2,40 +2,33 @@ import pytest
 from triangle import area, perimeter
 
 
-def test_triangle_area():
-    a, b, c = 6, 8, 10
-    result = area(a, b, c)
-    assert result == 24
+def test_area():
+    x, y, z = 5, 12, 13
+    res = area(x, y, z)
+    assert res == 30
+
+def test_perimeter():
+    x, y, z = 5, 12, 13
+    res = perimeter(x, y, z)
+    assert res == 30
 
 
-def test_area_with_zero_sides():
-    a, b, c = 0, 0, 0
-    result = area(a, b, c)
-    assert result == 0
+def test_perimeter_zero():
+    x, y, z = 0, 0, 0
+    res = perimeter(x, y, z)
+    assert res == 0
 
 
-def test_triangle_perimeter():
-    a, b, c = 6, 8, 10
-    result = perimeter(a, b, c)
-    assert result == 24
-
-
-def test_perimeter_with_zero_sides():
-    a, b, c = 0, 0, 0
-    result = perimeter(a, b, c)
-    assert result == 0
-
-
-def test_area_with_negative_sides():
-    a, b, c = -6, -8, -10
+def test_area_neg():
+    x, y, z = -5, -12, -13
     with pytest.raises(AssertionError):
-        area(a, b, c)
+        area(x, y, z)
 
 
-def test_perimeter_with_negative_sides():
-    a, b, c = -6, -8, -10
+def test_perimeter_neg():
+    x, y, z = -5, -12, -13
     with pytest.raises(AssertionError):
-        perimeter(a, b, c)
+        perimeter(x, y, z)
 
 def test_area_zero():
     x, y, z = 0, 0, 0
