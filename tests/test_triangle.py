7523babbcf71f755417f641ddf84e57a361d11c9
de0Gusman <471735@niuitmo.ru>
2024-12-18 19:15:37 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -7,15 +7,12 @@ def test_area():
     res = area(x, y, z)
     assert res == 30
 
+
 def test_area_zero():
     x, y, z = 0, 0, 0
     res = area(x, y, z)
     assert res == 0
 
-def test_area_zero():
-    x, y, z = 0, 0, 0
-    with pytest.raises(AssertionError):
-        area(x, y, z)
 
 def test_perimeter():
     x, y, z = 5, 12, 13
@@ -39,3 +36,8 @@ def test_perimeter_neg():
     x, y, z = -5, -12, -13
     with pytest.raises(AssertionError):
         perimeter(x, y, z)
+
+def test_area_zero():
+    x, y, z = 0, 0, 0
+    with pytest.raises(AssertionError):
+        area(x, y, z)
